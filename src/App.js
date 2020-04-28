d6d3b558f813ed187d59e7eebe5035a68768dc9f
Michael Michaud <mmichaud3@yahoo.com>
2020-04-28 10:18:00 -0700

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
 
+const ArtPage = () => (
+  <div>
+    <h1>ART PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/art" component={ArtPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,21 +14,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://cdn.pixabay.com/photo/2017/07/03/20/17/abstract-2468874_960_720.jpg",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "shop/art",
         },
         {
           title: "jewelry",
           imageUrl:
             "https://cdn.pixabay.com/photo/2015/03/09/13/56/colorful-665704__480.jpg",
           id: 2,
-          linkUrl: "shop/jackets",
+          linkUrl: "shop/jewelry",
         },
         {
           title: "sports memorabilia",
           imageUrl:
             "https://cdn.pixabay.com/photo/2015/05/14/09/43/backyard-baseball-766521__480.jpg",
           id: 3,
-          linkUrl: "shop/sneakers",
+          linkUrl: "shop/sports-memorabilia",
         },
         {
           title: "comics",
@@ -36,7 +36,7 @@ class Directory extends React.Component {
             "https://cdn.pixabay.com/photo/2017/06/11/23/48/thewalkingdead-2393903__480.jpg",
           size: "large",
           id: 4,
-          linkUrl: "shop/womens",
+          linkUrl: "shop/comics",
         },
         {
           title: "stamps",
@@ -44,7 +44,7 @@ class Directory extends React.Component {
             "https://cdn.pixabay.com/photo/2019/01/24/12/02/wallpaper-3952376__480.jpg",
           size: "large",
           id: 5,
-          linkUrl: "shop/mens",
+          linkUrl: "shop/stamps",
         },
       ],
     };
@@ -53,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
